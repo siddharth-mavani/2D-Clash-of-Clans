79b6d199ed24f3754409d709c82878e9e9933bbc
Siddharth Mavani <siddharth.mavani@students.iiit.ac.in>
2022-03-12 19:52:42 +0300

    changed file names

--- src/game.py
+++ src/game.py
@@ -1,79 +1,43 @@
+# Importing Libraries
+import os
+
 # Importing Custom Modules
+from input import input_to, Get
 from defs import *
-from huts import Hut
-from walls import Wall
-from townHall import TownHall
-from king import King, renderKingColor
-from cannon import Cannon, attackCannon
-from building import renderBuildingColor
-from barbarian import renderBarbarianColor, moveBarbarians, attackBarbarians
+from utils import Game
+from spells import castSpell
+from barbarian import spawnBarbarian
+from king import King, attackKing
 
-class Game:
-    def __init__(self):
-        # Initializing vars
-        self.board = []
-        self.state = True
-        self.activeBuildings = []
-        self.activeCannons = []
-        self.Barbarians = []
-        self.King = None
-        self.numActiveBuildings = 0
-        self.numActiveTroops = 0
-        self.result = None
+# Initializing Game
+game = Game()
 
-        # Calling init functions
-        self.initBoard()
-        TownHall.initTH(self)
-        Hut.initHut(self)
-        King.initKing(self)
-        Cannon.initCannon(self)
-        Wall.initWall(self)
+while(game.state):
+    # Printing Board
+    os.system('clear')
+    game.printBoard()
+
+    # Getting Input
+    get = Get()
+    key = input_to(get)
+
+    # Handling Input
+    if key == 'q':
+        game.state = False
+    elif key in ['1', '2', '3']:
+        spawnBarbarian(game, int(key))
+    elif key in ['w', 'a', 's', 'd']:
+        King.move(game,key)
+    elif key in ['r', 'h']:
+        castSpell(game, key)
+    elif key == ' ':
+        attackKing(game)
+        
+    game.update()
 
 
-    def initBoard(self):
-        for i in range(ROWS):
-            row = []
-            for j in range(COLS):
-                if(i == 0 and j == 0):
-                    row.append(TOP_LEFT_CORNER)
-                elif(i == 0 and j == COLS-1):
-                    row.append(TOP_RIGHT_CORNER)
-                elif(i == ROWS-1 and j == 0):
-                    row.append(BOTTOM_LEFT_CORNER)
-                elif(i == ROWS-1 and j == COLS-1):
-                    row.append(BOTTOM_RIGHT_CORNER)
-                elif(i == 0 or i == ROWS-1):
-                    row.append(TOP_WALL)
-                elif(j == 0 or j == COLS-1):
-                    row.append(SIDE_WALL)
-                else:
-                    row.append(BG)
-            row.append('\n')
-            self.board.append(row)
-    
-    def printBoard(self):
-
-        renderBuildingColor(self, self.activeBuildings)
-        renderKingColor(self, self.King)
-        renderBarbarianColor(self, self.Barbarians)
-
-        board = self.board
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                print(board[i][j], end='')
-
-        print("Active Troops: " + str(self.numActiveTroops))
-
-    def checkGame(self):
-        if(self.numActiveTroops == 0):
-            self.result = 'LOSE'
-            self.state = False
-        elif(self.numActiveBuildings == 0):
-            self.result = 'WIN'
-            self.state = False
-
-    def update(self):
-        moveBarbarians(self)
-        attackBarbarians(self)
-        attackCannon(self)
-        self.checkGame()
+os.system('clear')
+if(game.result == 'WIN'):
+    print('YOU WIN :)')
+elif(game.result == 'LOSE'):
+    print('YOU LOSE :(')
---
+++ src/utils.py
@@ -0,0 +1,79 @@
+# Importing Custom Modules
+from defs import *
+from huts import Hut
+from walls import Wall
+from townHall import TownHall
+from king import King, renderKingColor
+from cannon import Cannon, attackCannon
+from building import renderBuildingColor
+from barbarian import renderBarbarianColor, moveBarbarians, attackBarbarians
+
+class Game:
+    def __init__(self):
+        # Initializing vars
+        self.board = []
+        self.state = True
+        self.activeBuildings = []
+        self.activeCannons = []
+        self.Barbarians = []
+        self.King = None
+        self.numActiveBuildings = 0
+        self.numActiveTroops = 0
+        self.result = None
+
+        # Calling init functions
+        self.initBoard()
+        TownHall.initTH(self)
+        Hut.initHut(self)
+        King.initKing(self)
+        Cannon.initCannon(self)
+        Wall.initWall(self)
+
+
+    def initBoard(self):
+        for i in range(ROWS):
+            row = []
+            for j in range(COLS):
+                if(i == 0 and j == 0):
+                    row.append(TOP_LEFT_CORNER)
+                elif(i == 0 and j == COLS-1):
+                    row.append(TOP_RIGHT_CORNER)
+                elif(i == ROWS-1 and j == 0):
+                    row.append(BOTTOM_LEFT_CORNER)
+                elif(i == ROWS-1 and j == COLS-1):
+                    row.append(BOTTOM_RIGHT_CORNER)
+                elif(i == 0 or i == ROWS-1):
+                    row.append(TOP_WALL)
+                elif(j == 0 or j == COLS-1):
+                    row.append(SIDE_WALL)
+                else:
+                    row.append(BG)
+            row.append('\n')
+            self.board.append(row)
+    
+    def printBoard(self):
+
+        renderBuildingColor(self, self.activeBuildings)
+        renderKingColor(self, self.King)
+        renderBarbarianColor(self, self.Barbarians)
+
+        board = self.board
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                print(board[i][j], end='')
+
+        print("Active Troops: " + str(self.numActiveTroops))
+
+    def checkGame(self):
+        if(self.numActiveTroops == 0):
+            self.result = 'LOSE'
+            self.state = False
+        elif(self.numActiveBuildings == 0):
+            self.result = 'WIN'
+            self.state = False
+
+    def update(self):
+        moveBarbarians(self)
+        attackBarbarians(self)
+        attackCannon(self)
+        self.checkGame()
